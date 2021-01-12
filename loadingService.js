03ea21f590a52379117abab947162bb4570a3149
Irina Nikolaidi <irina.nikolaidi@rt.ru>
2021-01-12 10:55:37 +0300

    update master

--- loadingService.js
+++
@@ -1,9 +0,0 @@
-import { Subject } from 'rxjs';
-
-const loading$ = new Subject();
-
-export default {
-    showLoading: () => loading$.next(true),
-    hideLoading: () => loading$.next(false),
-    loadingStatus: loading$.asObservable(),
-}
