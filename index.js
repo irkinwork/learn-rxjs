373141f1acb271da83997211d822cf5261f47b35
Irina Nikolaidi <irina.nikolaidi@rt.ru>
2021-01-12 10:55:37 +0300

    update master

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-import loadingService from './loadingService';
+const observer = {
+    next: (v) => console.log('next', v),
+    error: (v) => console.log('error', v),
+    complete: (v) => console.log('complete', v),
+};
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-
-
- const loadingOverlay = document.getElementById('loading-layout');
-
-
- loadingService.loadingStatus.subscribe((isLoading) => {
-     if (isLoading) {
-        loadingOverlay.classList.add('open');
-     } else {
-        loadingOverlay.classList.remove('open');
-     }
- })
- loadingService.showLoading();
-
- setTimeout(() => {
-    loadingService.hideLoading()
- }, 3000)
