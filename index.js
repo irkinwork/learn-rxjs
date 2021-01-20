627f128a9678f2897d3e8b3c8c9dfc8b3b9fc1ed
Irina Nikolaidi <irina.nikolaidi@rt.ru>
2021-01-20 11:51:38 +0300

    finish store lab

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+import {StoreObservable} from './store';
 /*
  * Any code samples you want to play with can go in this file.
  * Updates will trigger a live reload on http://localhost:1234/
  * after running npm start.
  */
+
+const store = new StoreObservable({name: 'Irina', auth: true}); 
+store.selectState('name').subscribe(console.log)
+
+store.updateState({name: 'Irini', auth: true})
+store.updateState({name: 'Irini', auth: true})
+store.updateState({name: 'Irina', auth: true})
 
