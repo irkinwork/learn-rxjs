24d909cf524bb61166be03ad9f27db40accfa7ad
Irina Nikolaidi <irina.nikolaidi@rt.ru>
2021-01-21 11:26:28 +0300

    async subject

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+import {AsyncSubject} from 'rxjs';
+
 const observer = {
     next: (v) => console.log('next', v),
     error: (v) => console.log('error', v),
     complete: (v) => console.log('complete', v),
 };
 
+const subject = new AsyncSubject();
+
+subject.subscribe(observer)
+subject.subscribe(observer);
+
+subject.next('hello');
+subject.next('world');
+subject.next('goodbye');
+
+subject.complete('complete');
+
+// emit only last value
