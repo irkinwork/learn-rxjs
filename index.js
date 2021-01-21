ea495a8cb616d6ee386025ab12a618e305357d4b
Irina Nikolaidi <irina.nikolaidi@rt.ru>
2021-01-21 11:22:13 +0300

    share replay

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+import { fromEvent } from 'rxjs';
+import {ajax} from 'rxjs/ajax';
+import { mergeMapTo, shareReplay } from 'rxjs/operators';
+
 const observer = {
     next: (v) => console.log('next', v),
     error: (v) => console.log('error', v),
     complete: (v) => console.log('complete', v),
 };
 
+const ajax$ = ajax('https://api.github.com/users/octocat');
+
+const click$ = fromEvent(document, 'click');
+
+const clickRequest$ = click$.pipe(
+    mergeMapTo(ajax$),
+    shareReplay(1, 2000),
+);
+
+clickRequest$.subscribe(x => console.log('clickRequest', x))
+
+setTimeout(() => {
+    console.log('subscribing!')
+    clickRequest$.subscribe(x => console.log('clickRequest', x))
+}, 5000)
